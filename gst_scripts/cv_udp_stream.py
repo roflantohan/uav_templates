d4336b8d644ddec797cb336dad080e5cd3aa5d72
roflantohan <anton.bezz.v@gmail.com>
2025-01-30 15:43:15 +0200

    refactor: cv-gst udp stream

--- gst_scripts/cv_udp_stream.py
+++ gst_scripts/cv_udp_stream.py
@@ -1,13 +1,12 @@
+# Need to compile OpenCV with GStreamer support !
+
 import cv2
 import numpy as np
-from src.libs.shared_memory import SharedMemory
-
 
 class StreamUDP:
-    def __init__(self, shmem: SharedMemory):
-        self.shmem = shmem
-        self.host = self.shmem.config["udp_host"]
-        self.port = self.shmem.config["udp_port"]
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
         self.out: cv2.VideoWriter = None
 
         self.width = None
@@ -20,22 +19,15 @@ class StreamUDP:
             f"rtph264pay ! udpsink host={host} port={port}"
         )
         return cv2.VideoWriter(gst_pipeline, cv2.CAP_GSTREAMER, 0, fps, (w, h), True)
-
-    def wait_for_param(self):
-        while True:
-            self.width = self.shmem.read_dict("frame_width")
-            self.height = self.shmem.read_dict("frame_height")
-            if self.width and self.height:
-                break
-
-    def listen(self):
-        self.wait_for_param()
-
-        self.out = self.create_stream(self.host, self.port, self.width, self.height)
+    
+    def set_video_param(self, w, h):
+        self.width = w
+        self.height = h
         # gray background
         self.frame = np.full((self.width, self.height, 3), 128, dtype=np.uint8)
 
-        while True:
-            if not self.shmem.is_empty("frame_out"):
-                self.frame = self.shmem.get_queue("frame_out")
-            self.out.write(self.frame)
+    def set_frame(self, frame):
+        self.frame = frame
+
+    def send_frame(self):
+        self.out.write(self.frame)
